2120fd83aeec4f4d9ad9907fdf7fc9eb6e298893
BDKoussama <bdk.oussama@gmail.com>
2020-10-07 15:24:34 +0100

    added the variables scss file

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,7 +5,7 @@ const Header  = () => (
     <div className="header">
         <nav className = "navbar">
             <a href="#" className='logo'>
-                <span><bold>Github</bold> Jobs</span>
+                <span><strong>Github</strong> Jobs</span>
             </a>
         </nav>
     </div>  
