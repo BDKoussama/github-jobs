0b7defbe644206208a6dae5234520fd8835597cb
BDKoussama <bdk.oussama@gmail.com>
2020-10-10 16:53:40 +0100

    fixed footer position porblem

--- src/Components/JobsList.js
+++ src/Components/JobsList.js
@@ -9,6 +9,8 @@ const JobsList = () => {
                 <JobItem/>
                 <JobItem/>
                 <JobItem/>
+                <JobItem/>
+                <JobItem/>
             </ul>
         </div>
     )
